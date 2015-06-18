0f211f6910a98e92194192aa5e99b4573f7f939c
jmmarti <jmmartin612@gmail.com>
2015-06-18 10:31:52 -0400

    refactor for direction, distance, and audio components

--- Assets/Scripts/Data Components/DataController.cs
+++ Assets/Scripts/Data Components/DataController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StatusController : MonoBehaviour {
+public class DataController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Data Components/DataObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Data
+{
+	public class DataObject: MonoBehaviour 
+	{
+		public delegate void Subscribers(float[] output);
+		public Subscribers subscribers;
+
+		void Start()
+		{
+		}
+
+		void Update()
+		{
+		}
+
+		void PushUpdate()
+		{
+		}
+
+		public GameObject[] getPlayers()
+		{
+			return GameObject.FindGameObjectsWithTag ("Player");
+		}
+		
+	}
+}
