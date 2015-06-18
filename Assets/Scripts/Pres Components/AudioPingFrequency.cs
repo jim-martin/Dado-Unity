e0f462fb0065cb9c750abb51516a6c01b0e00dd5
Zachary Aman <zackyuaman@gmail.com>
2015-06-18 11:53:28 -0400

    stubbed out all audio recipes

--- Assets/Scripts/Pres Components/AudioPingFrequency.cs
+++ Assets/Scripts/Pres Components/AudioPingFrequency.cs
@@ -1,46 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AudioPingFrequency : PresentationObject
-{
+public class AudioPingFrequency : PresentationObject {
 	private Transform m_Transform;
 	public bool distance_input;
 	public bool direction_input;
 
 
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		Subscribe ();
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update () {
 	
 	}
 
-	public float get_interval ()
-	{
+	public float get_interval(){
 		float interval = 1;
 
 		Debug.Log (dataInput);
 
 		if (distance_input == true) {
-			float distance = dataInput [1];
-			Debug.Log (distance);
-			interval = distance / 50;
-		} else if (direction_input == true) {
 			float distance = dataInput [0];
 			Debug.Log (distance);
 			interval = distance / 50;
-		} 	
+		} else if (direction_input == true) {
 
-		if (interval < .1f) {
-			interval = .1f;
-		}
+		} 
+
+			if (interval < .1f) {
+				interval = .1f;
+			}
 		Debug.Log ("audio ping frequency:");
 		Debug.Log (interval);
-		return interval;
+			return interval;
 	}
 }
---
+++ Assets/Scripts/Pres Components/AudioPitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPitch : PresentationObject {
+
+	private Transform m_Transform;
+	public bool distance_input;
+	public bool direction_input;
+
+	// Use this for initialization
+	void Start () {
+		Subscribe ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float get_pitch(){
+		float pitch = 2.0f;
+
+		if (distance_input) {
+			//modulate pitch based on distance
+
+			pitch = 2.0f - distance / 20;
+			if (pitch < .12f) {
+				pitch = .12f;
+			}
+		} else if (direction_input){
+			//modulate pitch based on direction
+			pitch = 2.0f - Mathf.Abs(direction) / 180;
+			if (pitch < .12f) {
+				pitch = .12f;
+			}
+		}
+	
+		return pitch;
+	}
+}
