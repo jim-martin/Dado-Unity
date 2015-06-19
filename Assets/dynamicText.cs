1428ada9a8d0ae5c0b6928d840962da2c1646153
Alan Herman <aherman@andrew.cmu.edu>
2015-06-19 13:31:23 -0400

    pulled scripts from Scenario branch

---
+++ Assets/dynamicText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Data;
+using System.Collections;
+using System.Collections.Generic;
+
+public class dynamicText : MonoBehaviour {
+	Text text;  
+
+	// Use this for initialization
+	void Start () {
+		// Set up the reference.
+		text = GetComponent <Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] players = getPlayers ();
+			foreach (GameObject p in players) {
+		text.text = "Score";
+	}
+}
+
