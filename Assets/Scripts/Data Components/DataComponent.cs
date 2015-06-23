9115bee24f1cb7871c481cac80625ee8673ed21f
Alan Herman <aherman@andrew.cmu.edu>
2015-06-23 11:47:52 -0400

    functioning compass, still buggy though

--- Assets/Scripts/Data Components/DataComponent.cs
+++ Assets/Scripts/Data Components/DataComponent.cs
@@ -19,15 +19,26 @@ namespace Data
 
 			Vector3 tRot = target.transform.position - transform.position;
 
-			float angle = Vector3.Angle(target.transform.position, transform.forward);
-			int angleDir;
-			if (Vector3.Cross (target.transform.position, transform.forward).y > 0) {
+			Debug.Log (target.transform.position);
+
+			Vector2 target_vec = new Vector2 (getRelativePosition().x, getRelativePosition().z);
+//			Debug.Log (target_vec);
+			Vector2 facing_vec = new Vector2 (transform.forward.x, transform.forward.z);
+//			Debug.Log (transform.forward);
+//			Debug.Log (facing_vec);
+
+//			float angle = Vector3.Angle(getRelativePosition(), transform.forward);
+			float angle = Vector2.Angle(target_vec, facing_vec);
+//			Debug.Log (angle);
+			int angleDir = 1;
+			if (Vector3.Cross (getRelativePosition(), transform.forward).y > 0) {
 				angleDir = 1; //looking to the right of the target, pan to the left
 			} else {
 				angleDir = -1; //looking to the left of the target, pan to the right
 			}
-				
+
 			direction = angle*angleDir;
+			Debug.Log (direction);
 			return direction;
 		}
 		
--- Assets/canvasDynamics.cs
+++ Assets/canvasDynamics.cs
@@ -8,21 +8,36 @@ public class canvasDynamics : MonoBehaviour {
 	DataComponent data;
 	Canvas canvas;
 	Text text;  
+	/*private GameObject character;
+	  bool change;*/
+	private GameObject teammate;
+	private GameObject radar;
+	private GameObject playerAxis;
 
 	private float distance;
 	private float direction;
+	private float hudRadius;
+	private float proximityToTeammate = 187; // Almost all of radar radius
+	private float maxDistanceFromTeammate = 40;
+	private float prevDistance;
 
 	// Use this for initialization
 	void Start () {
 		data = GetComponent<DataComponent> ();
 		canvas = GetComponent<Canvas> ();
-		Debug.Log (canvas);
+		//Debug.Log (canvas);
 		//text = GetComponent<Text> ();\
 		GameObject textContainer = GameObject.Find("dynamicText");
+		teammate = GameObject.Find("teammate");
+		radar = GameObject.Find("radar");
+		playerAxis = GameObject.Find ("OVRCameraRig 1");
+		//character = GameObject.Find("character");
 		text = textContainer.GetComponent<Text> ();
-		Debug.Log( text );
-
+		//Debug.Log( text );
+		/*change = false;*/
 	//	text = GameObject.Find("dynamicText");
+		hudRadius = 200;
+		prevDistance = data.getDistance();
 	
 	}
 	
@@ -30,6 +45,44 @@ public class canvasDynamics : MonoBehaviour {
 	void Update () {
 		distance =  data.getDistance();
 		direction = data.getDirection();
-		text.text = "distance: " + distance;
+		Debug.Log (data.getGlobalPosition ());
+		Debug.Log (direction);
+		//text.text = "direction: " + direction + " distance: " + distance;
+		if (distance > prevDistance) {
+			//Debug.Log ("colder");
+			text.text = "colder";
+		} else {
+			//Debug.Log ("warmer");
+			text.text = "warmer";
+			prevDistance = distance;
+		}
+		//Debug.Log ("Distance: " + data.getDistance () + " prevDistance: " + prevDistance);
+		float difference = distance - prevDistance;
+		//Debug.Log ("Difference: " + difference);
+		if (distance - prevDistance > 2) {
+			prevDistance = distance;
+		}
+		/*if (change == true) {
+			character.transform.position = new Vector3 (character.transform.position.x + 2, character.transform.position.y, character.transform.position.z);
+			change = false;
+		} else {
+			character.transform.position = new Vector3 (character.transform.position.x - 2, character.transform.position.y, character.transform.position.z);
+			change = true;
+		}*/
+
+		// RADAR CODE
+		/*
+		 * Trigonometry stuff to rotate teammate
+		float y = Mathf.Sin (direction) * hudRadius;
+		float x = Mathf.Cos (direction) * hudRadius;
+		teammate.transform.position = new Vector3 (x, y, teammate.transform.position.z);
+		Debug.Log ("y: " + teammate.transform.position.y);
+		Debug.Log ("x: " + teammate.transform.position.x);
+		*/
+		proximityToTeammate = (distance * hudRadius) / maxDistanceFromTeammate;
+		//Debug.Log (playerAxis.transform.eulerAngles.y);
+		radar.transform.eulerAngles = new Vector3 (radar.transform.eulerAngles.x, radar.transform.eulerAngles.y, direction);
+		//teammate.transform.position = new Vector3 (teammate.transform.position.x, proximityToTeammate*0.003292852f, teammate.transform.position.z);
+		//radar.transform.Rotate (0, 0, direction);
 	}
 }
