ab1f4802b89b625213934812f29b4b35db4c1fea
Zachary Aman <zackyuaman@gmail.com>
2015-06-27 08:29:53 -0400

    bringing in min and max for scaling, z-axis variables

--- Assets/Scripts/Pres Components/Audio/AudioPitch.cs
+++ Assets/Scripts/Pres Components/Audio/AudioPitch.cs
@@ -6,6 +6,10 @@ public class AudioPitch : MonoBehaviour {
 	
 	public bool distance_input;
 	public bool direction_input;
+	public bool zAxis_input;
+	public float minimumPitchScale;
+	public float maximumPitchScale;
+
 	DataComponent data;
 
 	// Use this for initialization
@@ -22,20 +26,29 @@ public class AudioPitch : MonoBehaviour {
 	public float get_pitch(){
 		float pitch = 2.0f;
 
+		if (maximumPitchScale != null) {
+			pitch = maximumPitchScale;
+		}
+
 		if (distance_input) {
 			//modulate pitch based on distance
 
-			pitch = 2.0f - data.getDistance() / 20;
-			if (pitch < .12f) {
-				pitch = .12f;
-			}
+			pitch = pitch - data.getDistance() / 20;
+			//could use better calibration for distance - how far do we anticipate the maximum distance being?
+
 		} else if (direction_input){
 			//modulate pitch based on direction
 			pitch = 2.0f - Mathf.Abs(data.getDirection()) / 180;
-			if (pitch < .12f) {
-				pitch = .12f;
-			}
 		}
+
+		if (pitch < .12f) {
+			pitch = .12f;
+		}
+		if(pitch < minimumPitchScale){
+			pitch = minimumPitchScale;
+		}
+
+		//have pitch modulate up or down while playing in relation to change in distance
 	
 		return pitch;
 	}
