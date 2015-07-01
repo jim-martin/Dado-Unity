8dbc754388ce0a3ecd08d25ba3668bf1265aa947
jmmarti <jmmartin612@gmail.com>
2015-07-01 13:11:05 -0400

    Profile Save/Load in Inspector, new Practice space

---
+++ Assets/Editor/ProfileComponentEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(ProfileComponent))]
+public class ProfileComponentEditor : Editor {
+
+	public override void OnInspectorGUI(){
+
+		DrawDefaultInspector ();
+
+		ProfileComponent p = (ProfileComponent)target;
+
+		if (GUILayout.Button ("Save Profile")) {
+			p.saveProfile();
+		}
+		if (GUILayout.Button ("Load Profile")) {
+			p.loadProfile();
+		}
+	}
+}
