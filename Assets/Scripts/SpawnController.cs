55f05f108c8bebc9261a05280028f8b39ed35121
jmmarti <jmmartin612@gmail.com>
2015-07-08 23:40:49 -0400

    Exit is placed on player spawn location dynamically

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class SpawnController : MonoBehaviour {
 
+	public string tag;
+
 	GameController gc;
 	GameObject[] spawns;
 
 	void Awake (){
 
 		gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController> ();
-		spawns = GameObject.FindGameObjectsWithTag("player_spawn");
+		spawns = GameObject.FindGameObjectsWithTag(tag);
 
 //		for (int i = 0; i < spawns.Length; i++) {
 //			Debug.Log (spawns [i].name);
