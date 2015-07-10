67b96da55b2e0a137ca16b225f27fc4121cedd2d
jmmarti <jmmartin612@gmail.com>
2015-07-09 23:12:54 -0400

    for real, toggle tho

---
+++ Assets/ButtonToggle.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonToggle : MonoBehaviour {
+
+	public string profile;
+	bool on = false;
+
+	ProfileComponent player;
+	GameObject[] buttons;
+	Image img;
+	ButtonToggle tempButton;
+
+	// Use this for initialization
+	void Awake () {
+
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<ProfileComponent>();
+		buttons = GameObject.FindGameObjectsWithTag("practice_button");
+		img = GetComponent<Image>();
+	
+	}
+	
+	public void Toggle(){
+
+		on = !on;
+
+		if(on){
+
+			AddProfile();
+			//color button
+			img.fillCenter = true;
+
+		}else{
+
+			player.loadProfile("control");
+			for(int i = 0; i < buttons.Length; i++){
+				tempButton = buttons[i].GetComponent<ButtonToggle>();
+				if( tempButton.isOn() ){
+					tempButton.AddProfile();
+				}
+			}
+
+			//uncolor button
+			img.fillCenter = false;
+		}
+
+	}
+
+	public void AddProfile(){
+
+		player.loadProfile_Add(profile);
+
+	}
+
+	public bool isOn(){
+		return on;
+	}
+}
