e92c2819773d332767513574b427afd740a6cd1b
jmmarti <jmmartin612@gmail.com>
2015-07-10 02:25:53 -0400

    Fixed missing file error by remaking the goddamn file

---
+++ Assets/Scripts/PlayerPrefUpdater.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefUpdater : MonoBehaviour {
+
+
+	public int testStep = 0;
+	public int totalTestSteps = 4;
+	public string customProfile = "control";
+
+
+	public void UpdatePrefs(){
+
+		PlayerPrefs.SetInt("testStep", testStep);
+		PlayerPrefs.SetInt("totalTestSteps", totalTestSteps);
+		PlayerPrefs.SetString("customProfile", customProfile);
+
+	}
+}
