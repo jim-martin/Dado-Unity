36447b7249877b209a0f90443a7a819caa895562
jmmarti <jmmartin612@gmail.com>
2015-07-14 13:41:09 -0400

    Kicker fixed to align to view

--- Assets/Scripts/prefabs/KickController.cs
+++ Assets/Scripts/prefabs/KickController.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class KickController : MonoBehaviour {
 
+	Camera cam;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		transform.rotation = cam.transform.rotation;
 
 		if (Input.GetKeyDown (KeyCode.E)) {
 			GetComponent<Animator>().SetTrigger( "Trigger" );
